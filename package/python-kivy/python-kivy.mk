2cc1b88e0f58bb56473086bf441853386afa395d
Jérôme Quintard <jquintard@ubikyo.com>
2026-08-27 10:43:51 +0000

    Ajout plugin video

--- package/python-kivy/python-kivy.mk
+++ package/python-kivy/python-kivy.mk
@@ -16,15 +16,28 @@ PYTHON_KIVY_DEPENDENCIES = \
 	sdl2_image \
 	sdl2_mixer \
 	sdl2_ttf \
+	gstreamer1 \
+	gst1-plugins-base \
 	libgles \
 	libegl
 
 PYTHON_KIVY_ENV = \
 	PYTHONPATH="$(HOST_DIR)/lib/python-kivy:$(HOST_DIR)/lib/python3.14/site-packages" \
 	USE_SDL2=1 \
+	USE_GSTREAMER=1 \
 	USE_X11=0 \
 	USE_OPENGL_ES2=1 \
 	USE_OPENGL_MOCK=1
+
+define PYTHON_KIVY_NORMALIZE_GSTPLAYER_LINE_ENDINGS
+	$(SED) 's/\r$$//' \
+		$(@D)/kivy/core/video/video_gstplayer.py \
+		$(@D)/kivy/lib/gstplayer/_gstplayer.h \
+		$(@D)/kivy/lib/gstplayer/_gstplayer.pyx
+endef
+
+PYTHON_KIVY_POST_EXTRACT_HOOKS += \
+	PYTHON_KIVY_NORMALIZE_GSTPLAYER_LINE_ENDINGS
 
 define PYTHON_KIVY_RELAX_BUILD_DEPENDENCIES
 	$(SED) 's/setuptools~=69\.2\.0/setuptools>=69.2.0/' $(@D)/pyproject.toml
